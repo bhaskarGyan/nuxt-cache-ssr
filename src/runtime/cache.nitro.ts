d9ca45c690cb48f5792ce00bfa7959c84202c1d3
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-11-02 12:15:40 +0530

    Add gzip and brotli compression for response (#5)

--- src/runtime/cache.nitro.ts
+++ src/runtime/cache.nitro.ts
@@ -1,4 +1,5 @@
 import InMemoryCache from './cache.inmemory'
+import zlib from "node:zlib";
 import { options } from '#cache-ssr-options'
 import { isUrlCacheable } from './cache.utils'
 import type { NitroAppPlugin } from 'nitropack'
@@ -6,14 +7,50 @@ import generateFlags from './generateFlags'
 
 const customKey = options.key ? eval(options.key) : null
 
+const gzipOptions = { level: zlib.constants.Z_BEST_COMPRESSION }
+const brotliOptions = {
+  [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+  [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.Z_BEST_COMPRESSION,
+}
+
+const { encoding = '' } = options.compressResponse || {} as any
+
+const compressedBuff = (fileContents: string) => {
+  return new Promise((resolve, reject) => {
+    const cb = (error, result?: Buffer) => error ? reject(error) : resolve(result)
+    if (encoding === 'gzip') {
+      zlib.gzip(fileContents, gzipOptions, cb)
+    } else if (encoding === 'br') {
+      zlib.brotliCompress(fileContents, brotliOptions, cb)
+    } else {
+      cb('Invalid compression option. Please provide either br or gzip')
+    }
+  })
+}
+
 export default <NitroAppPlugin>async function (nitroApp) {
   const cacheOption = options.store || {};
-  await InMemoryCache.init(cacheOption)
+
+  await InMemoryCache.init(cacheOption);
+
   nitroApp.hooks.hook('render:response', async (response, { event }) => {
     const isCacheable = isUrlCacheable(event.req, options.pages)
+
     if (isCacheable && response.statusCode === 200) {
       const key = customKey ? customKey(event.req.url, event.req.headers, generateFlags(event.req.headers, event.req.headers['user-agent'])) : event.req.url
-      await InMemoryCache.set(key, response)
+      let cachedRes = response;
+
+      if (encoding) {
+        const encodedBuffer = await compressedBuff(response.body);
+
+        cachedRes = {
+          ...cachedRes, body: encodedBuffer, headers: {
+            ...cachedRes.headers, "content-encoding": encoding
+          }
+        }
+      }
+
+      await InMemoryCache.set(key, cachedRes);
 
     }
 
